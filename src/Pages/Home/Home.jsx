d3fc285eedca783f9a375873f5caa4d876f6251e
black-myke <michaelbolajoko@gmail.com>
2024-07-24 11:31:46 +0100

    Mobile Changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from "react";
-import "../Home/home.css"
-import { Container, Col, Row } from 'react-bootstrap'
+import "../Home/home.css";
+import { Container, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import github from "../../assets/Images/github2.svg"
-import html from "../../assets/Images/html-logo.svg"
-import css from "../../assets/Images/css-logo.svg"
-import bootstrap from "../../assets/Images/bootstrap.svg"
-import Profile from "../../assets/Images/avi.jpg"
-
+import github from "../../assets/Images/github2.svg";
+import html from "../../assets/Images/html-logo.svg";
+import css from "../../assets/Images/css-logo.svg";
+import bootstrap from "../../assets/Images/bootstrap.svg";
+import Profile from "../../assets/Images/avi.jpg";
+import About from "../AboutMe/About";
+import Skills from "../Skills/Skills";
+import Projects from "../Projects/Projects";
 
 const Home = () => {
-  const mailLink = "mailto:michaelbolajoko@gmail.com?subject=Job%20Proposal&body=Hi%20Michael, I have a job proposal!";
+  const mailLink =
+    "mailto:michaelbolajoko@gmail.com?subject=Job%20Proposal&body=Hi%20Michael, I have a job proposal!";
+  const Resume =
+    "https://drive.google.com/file/d/1x16rQEjht5B4Pc46K4n_C97Zz_OPYXF7/view";
 
-  const names = [
-    "React Developer",
-    "Web Designer",
-    "Frontend Web Developer",
-  ];
+  const names = ["React Developer", "Web Designer", "Frontend Web Developer"];
   const [currentName, setCurrentName] = useState(names[0]);
   const [index, setIndex] = useState(0);
 
@@ -32,7 +33,6 @@ const Home = () => {
     setCurrentName(names[index]);
   }, [index, names]);
 
-
   return (
     <div className="home-head">
       <div className="tools">
@@ -40,11 +40,9 @@ const Home = () => {
         <img className="css" src={css} alt="css" />
         <img className="html" src={html} alt="html" />
         <img className="bootstrap" src={bootstrap} alt="" />
-        
       </div>
 
       <Container fluid className="home-section">
-
         <Row className="home-main">
           <Col xs={12} lg={6} className="home-text">
             <h2 className="hi">Hi,</h2>
@@ -67,8 +65,17 @@ const Home = () => {
           </Col>
         </Row>
       </Container>
+
+      <div className="mobile-home">
+        <About />
+        <a href={Resume} className="mobile-resume" target="_blank">
+          Resume
+        </a>
+        <Skills />
+        <Projects />
+      </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
